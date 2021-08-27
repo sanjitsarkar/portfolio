e1d2a227e99e434ff23481a270a37c5fafee6d03
Sanjit Sarkar <ssanjit856@gmail.com>
2021-08-27 11:10:38 +0530

    added responsiveness

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const menu = document.querySelector('img.menu')
+const close = document.querySelector('img.close')
+const nav = document.querySelector('nav.right')
+menu.addEventListener('click',()=>{
+    nav.classList.toggle("show")
+    menu.classList.toggle("hide")
+    close.classList.toggle("hide")
+
+})
+close.addEventListener('click',()=>{
+    nav.classList.toggle("show")
+    close.classList.toggle("hide")
+    menu.classList.toggle("hide")
+
+})
+nav.addEventListener("click",()=>{
+    nav.classList.toggle("show")
+    close.classList.toggle("hide")
+    menu.classList.toggle("hide")
+})
