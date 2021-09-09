79d05103557b88d58fd0f0d07191f8f54227e369
Sanjit Sarkar <ssanjit856@gmail.com>
2021-09-09 21:28:51 +0530

    fixed className

--- pages/404.js
+++ pages/404.js
@@ -2,7 +2,7 @@ const NotFound = () => {
     return (
         <main className="not_found">
             {/* <h1>It feels like you have lost</h1> */}
-            <h1>4<span><span class="fas fa-ghost">0</span></span>4</h1>
+            <h1>4<span><span className="fas fa-ghost">0</span></span>4</h1>
   <h2>Error: 404 page not found</h2>
   <p>Sorry, the page you're looking for cannot be accessed</p>
         </main>
